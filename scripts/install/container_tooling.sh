85b9cc5f066f2cccaae884a5088b37593768f55f
Christof Weickhardt <christof@weickhardt.ch>
2022-06-03 17:34:06 +0000

    feat: fix podman

--- scripts/install/container_tooling.sh
+++ scripts/install/container_tooling.sh
@@ -3,34 +3,30 @@ set -e -x shl
 set -o pipefail
 . /etc/os-release
 
+# network things for rootless podman
+sudo curl -o /usr/bin/slirp4netns -fsSL https://github.com/rootless-containers/slirp4netns/releases/download/v1.1.12/slirp4netns-$(uname -m)
+sudo chmod +x /usr/bin/slirp4netns
+
 if [[ $VERSION_CODENAME == "focal" ]];
 then
-    echo "deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_${VERSION_ID}/ /" | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list \
-    && curl -L "https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_${VERSION_ID}/Release.key" | sudo apt-key add -\
-    && sudo apt-get install --reinstall ca-certificates \
-    && sudo mkdir /usr/local/share/ca-certificates/cacert.org \
-    && sudo wget -P /usr/local/share/ca-certificates/cacert.org http://www.cacert.org/certs/root.crt http://www.cacert.org/certs/class3.crt \
-    && sudo update-ca-certificates
+    echo "deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_${VERSION_ID}/ /" | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list
+    curl -L "https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_${VERSION_ID}/Release.key" | sudo apt-key add -
+    sudo apt-get install --reinstall ca-certificates
+    sudo mkdir /usr/local/share/ca-certificates/cacert.org
+    sudo wget -P /usr/local/share/ca-certificates/cacert.org http://www.cacert.org/certs/root.crt http://www.cacert.org/certs/class3.crt
+    sudo update-ca-certificates
 fi
 
 # install podman
-sudo apt-get update \
-    && sudo apt-get upgrade -y \
-    && sudo apt-get install podman -y
+sudo apt-get update
+sudo apt-get upgrade -y
+# needs to be installed before podman...
+sudo apt-get install -y fuse-overlayfs
+sudo apt-get install podman -y
 
 # setting up podman
-sudo cp /usr/share/containers/containers.conf /etc/containers/containers.conf \
-    && sudo sed -i '/^# cgroup_manager = "systemd"/ a cgroup_manager = "cgroupfs"' /etc/containers/containers.conf \
-    && sudo sed -i '/^driver = "overlay"/ c\driver = "vfs"' /etc/containers/storage.conf
+sudo cp /usr/share/containers/containers.conf /etc/containers/containers.conf
+sudo sed -i '/^# cgroup_manager = "systemd"/ a cgroup_manager = "cgroupfs"' /etc/containers/containers.conf
+sudo wget -O /etc/containers/storage.conf https://raw.githubusercontent.com/containers/podman/main/vendor/github.com/containers/storage/storage.conf
+sudo sed -i '/^driver = "overlay"/ c\driver = "vfs"' /etc/containers/storage.conf
     # && sed -i '/^# events_logger = "journald"/ a events_logger = "file"' /etc/containers/containers.conf \
-
-# docker
-sudo apt install apt-transport-https ca-certificates curl gnupg lsb-release -y
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg -y
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
-sudo apt update
-sudo apt -y install docker-ce docker-ce-cli containerd.io
-# setup docker buildx
-docker buildx create --use
-    
--- scripts/install/network_tooling.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e -x shl
-set -o pipefail
-
-# network things for thailscale etc.
-sudo curl -o /usr/bin/slirp4netns -fsSL https://github.com/rootless-containers/slirp4netns/releases/download/v1.1.12/slirp4netns-$(uname -m)
-sudo chmod +x /usr/bin/slirp4netns
