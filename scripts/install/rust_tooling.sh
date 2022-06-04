7608f076447c77d86bec557ea049b70b510e58d4
Christof Weickhardt <christof@weickhardt.ch>
2022-06-04 13:18:48 +0000

    feat: sync install

--- scripts/install/rust_tooling.sh
+++ scripts/install/rust_tooling.sh
@@ -17,3 +17,5 @@ install_rustup_channel_with_components beta-gnu &\
 install_rustup_channel_with_components nightly-gnu &\
 install_rustup_channel_with_components stable-gnu &\
 wait
+
+rustup target add wasm32-unknown-unknown
